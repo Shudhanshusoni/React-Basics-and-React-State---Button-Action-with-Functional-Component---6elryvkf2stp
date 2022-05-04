dd3b10bf8c8e079cbdea05d756017af78ed6ddd5
Shudhanshusoni <97749507+Shudhanshusoni@users.noreply.github.com>
2022-05-04 23:45:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,11 @@ import "./../styles/App.css";
 
 function App() {
   const [click,setClick]=useState();
-  const Text="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
   
   return (
     <div id="main">
       // Do not alter the main div
-      <p id="para">{Text}</p>
+      <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
       <button id='click' onClick={()=>setClick(!click)}>CLICK</button>
     </div>
   );
