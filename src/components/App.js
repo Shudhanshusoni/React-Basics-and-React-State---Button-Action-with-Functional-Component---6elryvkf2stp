61fac53636de914d1efb504087033f24d7205b31
Shudhanshusoni <97749507+Shudhanshusoni@users.noreply.github.com>
2022-05-04 23:40:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,14 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  const param=()=>{
-    <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-  }
+  const [click,setClick]=useState();
+  const Text="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+  
   return (
     <div id="main">
       // Do not alter the main div
-      
-      <button id='click' onClick={param}>CLICK</button>
+      <p id="para">{Text}</p>
+      <button id='click' onClick={()=>setClick(!click)}>CLICK</button>
     </div>
   );
 }
