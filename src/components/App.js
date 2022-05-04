ced5dd69ca678641632adf2b2752dde811255e66
Shudhanshusoni <97749507+Shudhanshusoni@users.noreply.github.com>
2022-05-04 14:43:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,11 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  const Para=document.getElementById("para")
   return (
     <div id="main">
       // Do not alter the main div
       <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-      <button onClick={()=>'Para'}>CLICK</button>
+      <button id='click' onClick="para">CLICK</button>
     </div>
   );
 }
